3a329d7b015a4d96d9da88521f0234660cd64b21
Mohamed Adel <72199655+MohamedAdelLotfi@users.noreply.github.com>
2022-11-07 11:16:46 +0200

    v1 - remove unused imports

--- drones/src/main/java/com/musala/drones/service/mapper/MedicationMapper.java
+++ drones/src/main/java/com/musala/drones/service/mapper/MedicationMapper.java
@@ -2,10 +2,7 @@ package com.musala.drones.service.mapper;
 
 import com.musala.drones.domain.Medication;
 import com.musala.drones.service.dto.MedicationDTO;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring", uses = {})
 public interface MedicationMapper extends EntityMapper<MedicationDTO, Medication> {
